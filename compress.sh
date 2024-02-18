b3332d006fcc6830f3e359170c8878e399dedc78
David Lang <davidlang42@gmail.com>
2024-02-18 17:33:01 +1100

    Action future ideas

--- compress.sh
+++ compress.sh
@@ -3,6 +3,8 @@
 
 #TODO remove existing files in output if they don't exist in input anymore
 
+#FUTURE check for duplicate input files
+
 # make all required dirs if they don't already exist
 (cd $INPUT_RAW_PATH && find . -mindepth 1 -type d -exec mkdir -p "$OUTPUT_COMPRESSED_PATH/{}" ';')
 
@@ -10,10 +12,9 @@
 find $INPUT_RAW_PATH -mindepth 1 -type f -iname "*.pdf" -printf '%P\n' | parallel -- /compress_pdf.sh {}
 
 #TODO trigger audio re-encoding: m4a, mp3, mpga, wma, aiff
-#TODO trigger video re-encoding: mp4, mkv, m4v
-#TODO convert word documents to PDF: doc, docx, odt
-#TODO convert music files to PDF: mus, musx, mxl, sib
-#TODO convert images to PDF: jpg, jpeg, tif, png
-#TODO zip FOLDERS which are named "*.zip" into zip files
+#FUTURE trigger video re-encoding: mp4, mkv, m4v
+#FUTURE trigger image re-encoding: jpg, jpeg, tif, png
+#FUTURE convert word documents to PDF: doc, docx, odt
+#FUTURE zip FOLDERS which are named "*.zip" into zip files
 
 #TODO remove empty dirs in OUTPUT_COMPRESSED_PATH
