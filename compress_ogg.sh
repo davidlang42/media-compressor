6d757d5a33589d16b0d1cef02f686e56d40c7a41
David Lang <davidlang42@gmail.com>
2024-02-18 19:38:35 +1100

    Fix escaping bug

--- compress_ogg.sh
+++ compress_ogg.sh
@@ -15,7 +15,7 @@ FILE_NO_EXT="$(echo "$1" | rev | cut -f 2- -d '.' | rev)"
 # compress if not found
 if ! [ -f "$OUTPUT_COMPRESSED_PATH/$FILE_NO_EXT.ogg" ]
 then
-    ffmpeg -hide_banner -loglevel error -i $INPUT_RAW_PATH/$1 $OGG_FFMPEG_ARGS $OUTPUT_COMPRESSED_PATH/$FILE_NO_EXT.tmp.ogg # must end in .ogg for ffmpeg to know what to do
+    ffmpeg -hide_banner -loglevel error -i "$INPUT_RAW_PATH/$1" $OGG_FFMPEG_ARGS "$OUTPUT_COMPRESSED_PATH/$FILE_NO_EXT.tmp.ogg" # must end in .ogg for ffmpeg to know what to do
     mv "$OUTPUT_COMPRESSED_PATH/$FILE_NO_EXT.tmp.ogg" "$OUTPUT_COMPRESSED_PATH/$FILE_NO_EXT.ogg"
     FROM_SIZE=$(ls -lh "$INPUT_RAW_PATH/$1" | cut -d' ' -f5)
     TO_SIZE=$(ls -lh "$OUTPUT_COMPRESSED_PATH/$FILE_NO_EXT.ogg" | cut -d' ' -f5)
